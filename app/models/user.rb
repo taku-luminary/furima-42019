2103660e655f251ccea2dac8573d2b9bdb250b27
Takuma Awaki <takuma@TakumanoMacBook-Air.local>
2025-03-30 00:10:04 +0900

    ユーザー管理機能1回目

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Associations
+  has_many :items
+  has_many :purchases
+
+  # Validations
+  validates :nick_name, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: 'は英字と数字の両方を含めて設定してください' }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/, message: 'は全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ンー]+\z/, message: 'は全角カタカナで入力してください' }
+end
